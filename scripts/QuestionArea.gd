f9ce3c75c2415ccca52abd93582e5dc23a1b61b2
SobyDamn <sobydanny@gmail.com>
2020-08-27 19:27:57 +0530

    added question

--- scripts/QuestionArea.gd
+++ scripts/QuestionArea.gd
@@ -2,13 +2,11 @@ extends Node2D
 
 onready var http : HTTPRequest = $FetchQuestions
 onready var userDetail = Firebase.userDetail
-var questionsList = []
+var questionData = {} #will get the value from utility
 func _ready():
+	for button in get_tree().get_nodes_in_group("OptionButton"):
+		button.connect("pressed", self, "option_pressed", [button])
 	Firebase.get_document(http)
-	
-
-
-
 func _on_FetchQuestions(result, response_code, headers, body):
 	var response_body := JSON.parse(body.get_string_from_ascii())
 	if response_code != 200:
@@ -23,9 +21,25 @@ func _on_FetchQuestions(result, response_code, headers, body):
 			Firebase.get_document(http,"questions?pageSize=20&pageToken=%s"%nextPage)
 		else:
 			nextPage=false
-		#questionsList = docs.fields
 		for i in range(0,docs.size()):
 			Utility.QuestionList.append(docs[i])
 		if not nextPage:
 			print(Utility.QuestionList.size())
-			Utility.fetchQuestion()
+			askQuestion()
+func askQuestion():
+	print("Fetching next question")
+	questionData = Utility.fetchQuestion()
+	$QuestionContainer/Question.text = questionData.question
+	$OptionContainer/OptionA.text = questionData.option[0]
+	$OptionContainer/OptionB.text = questionData.option[1]
+	$OptionContainer/OptionC.text = questionData.option[2]
+	$OptionContainer/OptionD.text = questionData.option[3]
+	print(questionData)
+
+
+func option_pressed(button):
+	if button.text == questionData.answer:
+		print("Asking next question")
+		askQuestion()
+	else:
+		print("wrong")
--- scripts/Utility.gd
+++ scripts/Utility.gd
@@ -1,26 +1,32 @@
 extends Node
 
 var QuestionList = []
+var alreadyAsked = []
 var questionInteracted = 0 #number of question interacted with
 var temp = {
 	"Hard":0,
 	"Normal":0,
 	"Easy":0
 }
-func fetchQuestion():
+func fetchQuestion(idVal:String = "",difficultyType:String="Easy"):
 	for i in range(0,QuestionList.size()):
 		var difficulty = QuestionList[i].fields.type.stringValue
 		var id = queID(QuestionList[i].name)
-		var questionData = {}
-		questionData.question = QuestionList[i].fields.question.stringValue
-		questionData.answer = QuestionList[i].fields.answer.stringValue
-		questionData.type = difficulty
-		questionData.option = [QuestionList[i].fields.option.arrayValue.values[0].stringValue,QuestionList[0].fields.option.arrayValue.values[1].stringValue,QuestionList[0].fields.option.arrayValue.values[2].stringValue,QuestionList[0].fields.option.arrayValue.values[3].stringValue]
-		questionData.id = id
-		temp.difficulty +=1
-		#print(questionData)
-		#return questionData
-	return {}
+		var hadAsked = id in alreadyAsked
+		if not hadAsked && difficulty == difficultyType:
+			alreadyAsked.append(id)
+			var questionData = {}
+			questionData.question = QuestionList[i].fields.question.stringValue
+			questionData.answer = QuestionList[i].fields.answer.stringValue
+			questionData.type = difficulty
+			questionData.option = [QuestionList[i].fields.option.arrayValue.values[0].stringValue.strip_edges(),QuestionList[i].fields.option.arrayValue.values[1].stringValue.strip_edges(),QuestionList[i].fields.option.arrayValue.values[2].stringValue.strip_edges(),QuestionList[i].fields.option.arrayValue.values[3].stringValue.strip_edges()]
+			questionData.id = id
+			questionInteracted +=1
+			print(questionInteracted,"Question asked")
+			return questionData
+		else:
+			questionInteracted +=1
+	return null
 func queID(url_str: String) -> String:
 	var strArray = url_str.split("/")
 	return strArray[strArray.size()-1]
